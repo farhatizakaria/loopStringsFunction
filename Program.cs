e08b6ebc85aa1a79fc61a5fba3d4a5f47fc92857
Zakaria Farhati <zakariafarhati18@gmail.com>
2020-01-20 19:30:39 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace giraffe
+{
+    class Program
+    {
+        public static void reLoop(string loopstring, int count)
+        {
+             Console.WriteLine(String.Concat(Enumerable.Repeat(loopstring,count)));
+        }
+        static void Main(string[] args)
+        {
+            // Exemple
+            reLoop("zakaria ", 3);
+            reLoop("Hello world ", 10);
+            Console.ReadKey();
+        }
+    }
+}
